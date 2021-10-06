77d343b67c4b18bf0587e9dfef99d640471a9887
xuanlongmmo <xuanlongmmo@gmail.com>
2021-10-06 09:56:26 +0700

    upload code

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('address_admin')->nullable();
             $table->string('password');
             $table->rememberToken();
